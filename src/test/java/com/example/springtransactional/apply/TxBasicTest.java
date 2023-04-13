461b9b33f1c12ed5fc1754519445f280bbc2314e
minseok <alstjr706@naver.com>
2023-04-14 02:49:46 +0900

    feat : 트랜잭션 설명

---
+++ src/test/java/com/example/springtransactional/apply/TxBasicTest.java
@@ -0,0 +1,11 @@
+package com.example.springtransactional.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+
+}
