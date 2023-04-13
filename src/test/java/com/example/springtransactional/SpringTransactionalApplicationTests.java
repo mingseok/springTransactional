2d25871cf3e2d1e26922a9ad54e8017cd1f684e4
minseok <alstjr706@naver.com>
2023-04-13 20:31:11 +0900

    Initial commit

---
+++ src/test/java/com/example/springtransactional/SpringTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
